a974ef5596bace1aff7bd187416fe011d251ebf6
Nicolas <TheGreenkey@users.noreply.github.com>
2020-09-05 18:47:00 +0200

    use the os path segment separator

--- reloader.js
+++ reloader.js
@@ -1,11 +1,11 @@
 /// <reference path="node_modules/@citizenfx/server/index.d.ts" />
 const chokidar = require('chokidar');
-const { parse, extname } = require('path');
+const { parse, extname, sep } = require('path');
 
 chokidar.watch('resources').on('change', (path) => {
   if (!['.dll', '.js', '.lua'].includes(extname(path))) return; //? ensure the filetype matches one of the three default extensions
   const parsedPath = parse(path);
-  const resourceFromPath = parsedPath.dir.split('\\');
+  const resourceFromPath = parsedPath.dir.split(sep);
   const foundRes = resourceFromPath.find((path) => GetResourcePath(path) !== null);
   if (GetCurrentResourceName() === foundRes) return; //? Make sure that the resource we want to restart isn't ourself
   ExecuteCommand(`ensure ${foundRes}`);
